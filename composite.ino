52c31ed7c1a089f05527b8ee03414b4dc1f41270
CodeIGuess <46802090+CodeIGuess@users.noreply.github.com>
2020-04-03 14:29:29 -0400

    Update composite.ino

--- composite.ino
+++ composite.ino
@@ -1,41 +1,34 @@
-#define OUT 12
+#define HS D3
+#define VS D4
+#define VF D5
+#define VD D6
 
+int m;
 void setup() {
-  pinMode(OUT, OUTPUT);
+  pinMode(HS, OUTPUT);
+  pinMode(VS, OUTPUT);
+  pinMode(VF, OUTPUT);
+  pinMode(VD, OUTPUT);
 }
-
 void loop() {
-  line();
-}
-
-void line() {
-
-  // Back low
-  analogWrite(OUT, 0);
-  int mc = micros();
-  while (micros() < mc+5) {};
-  
-  // Back high
-  analogWrite(OUT, 93);
-  int mc = micros();
-  while (micros() < mc+5) {};
-  
-  
-  // Data (noise)
-  int mc = micros();
-  while (micros() < mc+5) {
-    analogWrite(OUT, random(93, 310));
-  };
-  
-  
-  // Front low
-  analogWrite(OUT, 0);
-  int mc = micros();
-  while (micros() < mc+5) {};
-  
-  // Front high
-  analogWrite(OUT, 93);
-  int mc = micros();
-  while (micros() < mc+5) {};
- 
+  digitalWrite(VS, HIGH);
+  for (int y = 0; y < 480; y++) {
+    digitalWrite(HS, HIGH);
+    digitalWrite(VF, HIGH);
+    m = micros(); while (micros() < m+25) {analogWrite(VD, 217);}
+    digitalWrite(VD, 0);
+    m = micros(); while (micros() < m+1) {}
+    digitalWrite(HS, LOW);
+    m = micros(); while (micros() < m+4) {}
+    digitalWrite(HS, HIGH);
+    m = micros(); while (micros() < m+2) {}
+    digitalWrite(VF, LOW);
+  }
+  m = micros(); while (micros() < m+450) {}
+  digitalWrite(VS, LOW);
+  m = micros(); while (micros() < m+64) {}
+  digitalWrite(VS, HIGH);
+  m = micros(); while (micros() < m+1020) {
+    // Calculate
+  }
 }
