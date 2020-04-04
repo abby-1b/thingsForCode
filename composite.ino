56589fc8179fce674d208814c4321ca8d908bad1
CodeIGuess <46802090+CodeIGuess@users.noreply.github.com>
2020-04-04 08:52:16 -0400

    Update composite.ino

--- composite.ino
+++ composite.ino
@@ -1,39 +1,26 @@
-// D3
-#define HS 0
 // D5
-#define VS 14
-// D6
-#define VF 12
-// D7
-#define VD 13
+#define OUT 14
 
+// Microsecond store variable
 int m;
 
 void setup() {
-  pinMode(HS, OUTPUT);
-  pinMode(VS, OUTPUT);
-  pinMode(VF, OUTPUT);
-  pinMode(VD, OUTPUT);
+  pinMode(OUT, OUTPUT);
 }
+
 void loop() {
-  digitalWrite(VS, HIGH);
-  for (int y = 0; y < 480; y++) {
-    digitalWrite(HS, HIGH);
-    digitalWrite(VF, HIGH);
-    m = micros(); while (micros() < m+25) {analogWrite(VD, 217);}
-    digitalWrite(VD, 0);
-    m = micros(); while (micros() < m+1) {}
-    digitalWrite(HS, LOW);
-    m = micros(); while (micros() < m+4) {}
-    digitalWrite(HS, HIGH);
-    m = micros(); while (micros() < m+2) {}
-    digitalWrite(VF, LOW);
+  // Back porch
+  analogWrite(OUT, 0);
+  m = micros()+5; while (micros() < m) {};
+  analogWrite(OUT, 80);
+  m = micros()+5; while (micros() < m) {};
+
+  // Video data
+  m = micros()+42; while (micros() < m) {
+    analogWrite(OUT, 280);
   }
-  m = micros(); while (micros() < m+450) {}
-  digitalWrite(VS, LOW);
-  m = micros(); while (micros() < m+64) {}
-  digitalWrite(VS, HIGH);
-  m = micros(); while (micros() < m+1020) {
-    // Calculate
-  }
+ 
+  // Front porch
+  analogWrite(OUT, 80);
+  m = micros()+11; while (micros() < m) {};
 }
