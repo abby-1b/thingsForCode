0b137cb052cb284cbee6973b332bbe2a5f7007cb
CodeIGuess <46802090+CodeIGuess@users.noreply.github.com>
2020-05-03 16:36:16 -0400

    Update js.js

--- js.js
+++ js.js
@@ -1,1 +1,17 @@
-// JS File
+themeColor(Color(0));
+
+
+useColor(Color(255))
+
+touchLen = 0
+touches = [];
+
+
+
+setInterval(function(){
+  background(Color(0));
+
+  text(buttons+'', 2, 2);
+  //console.log(touches);
+
+},100);
