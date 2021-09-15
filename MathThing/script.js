eca9e95c602e5a795da3862545f238744bba9fa3
CodeIGuess <codeiguess@gmail.com>
2021-09-15 16:13:34 -0400

    Graphs, paste testing, different colors, and no repeats

--- MathThing/script.js
+++ MathThing/script.js
@@ -1,15 +1,19 @@
 
 // Mode
-let mode = 0
+let mode = false
+let metaCtrlDown = 0
 
 // Graph drawing things
 let graphScale = 31 // How much to scale the graph
 let graphStep = 5 // Steps to take when drawing graph
+let colors = ["#ba2613", "#2cba13", "#1374ba", "#b7ba13", "#13ba88", "#6f13ba"]
 
 let math = {
     sin: Math.sin,
     cos: Math.cos,
-    round: Math.round
+    round: Math.round,
+    bin: (n) => { console.log("BIN!"); return parseInt(n + "", 2) },
+    log: (n) => { console.log(n) }
 }
 
 // Canvas
@@ -63,89 +67,136 @@ function process(js, context) {
 }
 
 // Guess what this does...
-function drawGraph(g, v, context) {
+function drawGraph(g, v, context, id) {
+    cvc.strokeStyle = colors[id % colors.length]
+    cvc.beginPath()
     try {
         let l = 0;
         for (let x = -cv.width / 2 - graphStep; x < cv.width / 2 + graphStep; x += graphStep) {
             let y = function(){return eval("with(storeVars(this)){" + g.replace(new RegExp(v, 'g'), x / graphScale) + "}")}.call(context) * graphScale
-            cvc.strokeStyle = "#f00"
             cvc.moveTo(x, -y)
             cvc.lineTo((x - graphStep), -l)
             l = y
             cvc.stroke()
         }
     } catch (e) {}
+    cvc.closePath()
+}
+
+// Updates the textarea
+function updateInfo() {
+    document.getElementById('if').value = `Tab Menu
+    
+    Press Tab again to exit
+    Press  Up  /  Down  to change graph sizing ±1
+    Press Left / Right  to change graph sizing ±5`
 }
 
 document.onkeydown = function(e){
-    // Special keys
-    if (e && e.key == "Tab") {
-        e.preventDefault()
-        mode = (mode + 1) % 2
-        return
-    }
+    if (!e) return
+    if (e.key) {
+        // Special keys
+        if (e.key == "Tab") {
+            e.preventDefault()
+            mode = !mode
+            document.getElementById('if').style.opacity = mode ? '1' : '0'
+            return
+        }
 
-    if (mode == 1) {
+        if (mode) {
+            let act = false
+            if (e.key == "ArrowUp") {
+                act = true; graphScale += 1
+            } else if (e.key == "ArrowDown") {
+                act = true; graphScale -= 1
+            } else if (e.key == "ArrowLeft") {
+                act = true; graphScale -= 5
+            } else if (e.key == "ArrowRight") {
+                act = true; graphScale += 5
+            }
+            if (act) setTimeout(mathAnalyze, 0)
+            e.preventDefault()
+            return
+        }
 
-        return
+        metaCtrlDown |= (e.key == "Meta"    ? 1 : 0)
+        metaCtrlDown |= (e.key == "Control" ? 2 : 0)
+
+        if (metaCtrlDown != 0 && e.key == 'z') setTimeout(mathAnalyze, 1)
+        if (e.key.includes("Arrow") || e.key == "Shift" || e.key == "Control" || e.key == "Meta" || e.key == "Alt" || e.key == "CapsLock")
+            return
+    } else {
+        console.log(e)
     }
 
     // This timeout makes sure the code runs after the key is processed by the browser.
-    setTimeout(function mathAnalyze(){
-        let str = document.getElementById("edt").innerText
-        while (str[str.length - 1] == '\n') str = str.substring(0, str.length - 1)
-        str = str.replace(/\n\n/g, "\n")
-        str = str.split("\n")
-        let ov = document.getElementById("ov")
-        for (let ne = ov.children.length; ne < str.length; ne++) ov.appendChild(document.createElement("div"))
-        let ctx = {}
-        let graphs = []
-        for (let s = 0; s < ov.children.length; s++) {
-            if (s >= str.length) { ov.children[s].innerHTML = "&nbsp;"; continue }
-            if (str[s].length == 0) { ov.children[s].innerHTML = "&nbsp;"; continue }
-
-            if (str[s][str[s].length - 1] == '\\' || str[s][str[s].length - 1] == '=') {
-                // Math: Implements multiplication without an operator.
-                ctx = mathProcess(str[s].slice(0,-1), ctx)
-                ov.children[s].innerHTML = "&nbsp;".repeat(str[s].length + 1) + returned
-            } else if (str[s][str[s].length - 1] == '~') {
-                // Execution: Executes JS code. No questions asked.
-                ctx = process(str[s].slice(0, -1), ctx)
-                ov.children[s].innerHTML = "&nbsp;".repeat(str[s].length + 1) + returned
-            } else if (str[s][str[s].length - 1] == '$') {
-                // Graph: Graphs things to a canvas.
-                graphs.push(parseMath(str[s].slice(0,-1)))
-                ov.children[s].innerHTML = "&nbsp;".repeat(str[s].length + 1) + "Graphed"
-            } else {
-                // Didn't find anything to do.
-                ov.children[s].innerHTML = "&nbsp;" // If there's nothing (a normal space is nothing) the line doesn't render.
-            }
-        }
-        cv.width = cv.width
-        cvc.translate(Math.floor(cv.width / 2) + .5, Math.floor(cv.height / 2) + .5)
-        cvc.strokeStyle = "#000"
-        cvc.moveTo(cv.width / 2, 0)
-        cvc.lineTo(-cv.width / 2, 0)
-        cvc.stroke()
-        cvc.moveTo(0, cv.height / 2)
-        cvc.lineTo(0, -cv.height / 2 - .5)
-        cvc.stroke()
-        if (graphs.length > 0) {
-            cv.style.opacity = "1"
-            cv.style.backgroundColor = "rgba(255,255,255,1)"
-            for (let g = 0; g < graphs.length; g++) {
-                graphs[g] = graphs[g].split('=')
-                if (graphs[g][0] == "y") {
-                    drawGraph(graphs[g][1], 'x', ctx)
-                }
-            }
-        } else {
-            cv.style.opacity = "0"
-            cv.style.backgroundColor = "rgba(255,255,255,0)"
-        }
-    }, 0)
+    setTimeout(mathAnalyze, 0)
 }
 document.onpaste = document.onkeydown
+
+document.onkeyup = function(e) {
+    metaCtrlDown ^= (e.key == "Meta"    ? 1 : 0)
+    metaCtrlDown ^= (e.key == "Control" ? 2 : 0)
+}
+
+function mathAnalyze() {
+    let str = document.getElementById("edt").value
+    while (str[str.length - 1] == '\n') str = str.substring(0, str.length - 1)
+    str = str.split("\n")
+    let ov = document.getElementById("ov")
+    for (let ne = ov.children.length; ne < str.length; ne++) ov.appendChild(document.createElement("div"))
+    let ctx = {m: {x: 1, y: 1}}
+    
+    // Graph resetting
+    let graphNum = 0
+    let graphEnabled = false
+    cv.width = cv.width
+    cvc.translate(Math.floor(cv.width / 2) + .5, Math.floor(cv.height / 2) + .5)
+    cvc.strokeStyle = "#000"
+    cvc.moveTo(cv.width / 2, 0)
+    cvc.lineTo(-cv.width / 2, 0)
+    cvc.stroke()
+    cvc.moveTo(0, cv.height / 2)
+    cvc.lineTo(0, -cv.height / 2 - .5)
+    cvc.stroke()
+
+    for (let s = 0; s < ov.children.length; s++) {
+        if (s >= str.length) { ov.children[s].innerHTML = "&nbsp;"; continue }
+        if (str[s].length == 0) { ov.children[s].innerHTML = "&nbsp;"; continue }
+
+        if (str[s][str[s].length - 1] == '\\' || str[s][str[s].length - 1] == '=') {
+            // Math: Implements multiplication without an operator.
+            ctx = mathProcess(str[s].slice(0,-1), ctx)
+            if ([undefined, NaN, null].includes(returned)) returned = "!!!"
+            ov.children[s].innerHTML = "&nbsp;".repeat(str[s].length + 1) + returned
+        } else if (str[s][str[s].length - 1] == '~') {
+            // Execution: Executes JS code. No questions asked.
+            ctx = process(str[s].slice(0, -1), ctx)
+            if ([undefined, NaN, null].includes(returned)) returned = "!!!"
+            ov.children[s].innerHTML = "&nbsp;".repeat(str[s].length + 1) + returned
+        } else if (str[s][str[s].length - 1] == '$') {
+            // Graph: Graphs things to a canvas.
+            let gstr = parseMath(str[s].slice(0,-1)).split('=')
+            if (gstr.length == 1 && gstr[0] == "") graphEnabled = true
+            if (gstr[0] == "y") {
+                drawGraph(gstr[1], 'x', ctx, graphNum)
+            }
+            ov.children[s].innerHTML = "&nbsp;".repeat(str[s].length + 1) + (gstr[0] == '' ? "Graph enabled" : (graphEnabled ? "Graphed" : "Off"))
+        } else {
+            // Didn't find anything to do.
+            ov.children[s].innerHTML = "&nbsp;" // If there's nothing (a normal space is nothing) the line doesn't render.
+        }
+    }
+
+    // Show/hide graphs
+    if (graphEnabled) {
+        cv.style.opacity = "1"
+        cv.style.backgroundColor = "rgba(255,255,255,1)"
+    } else {
+        cv.style.opacity = "0"
+        cv.style.backgroundColor = "rgba(255,255,255,0)"
+    }
+}
 
 window.onresize = function() {
     cv.width = window.innerWidth / 2
@@ -153,5 +204,7 @@ window.onresize = function() {
 }
 window.onresize()
 
-
 setTimeout(document.onkeydown, 100)
+
+updateInfo()
+
