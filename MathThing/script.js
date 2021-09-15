bb201812cb6bfe149ca61e7da232e67b9d51f517
CodeIGuess <codeiguess@gmail.com>
2021-09-15 12:53:14 -0400

    Made graphs!

--- MathThing/script.js
+++ MathThing/script.js
@@ -1,4 +1,17 @@
 
+// Mode
+let mode = 0
+
+// Graph drawing things
+let graphScale = 31 // How much to scale the graph
+let graphStep = 5 // Steps to take when drawing graph
+
+let math = {
+    sin: Math.sin,
+    cos: Math.cos,
+    round: Math.round
+}
+
 // Canvas
 let cv = document.getElementById("cv")
 let cvc = cv.getContext('2d')
@@ -8,7 +21,7 @@ let returned = ""
 function storeVars(target) {
     return new Proxy(target, {
         has(target, prop) { return true },
-        get(target, prop) { return (prop in target ? target : window)[prop] }
+        get(target, prop) { return (prop in target ? target : math)[prop] }
     })
 }
 
@@ -50,11 +63,33 @@ function process(js, context) {
 }
 
 // Guess what this does...
-function drawGraph() {
-    
+function drawGraph(g, v, context) {
+    try {
+        let l = 0;
+        for (let x = -cv.width / 2 - graphStep; x < cv.width / 2 + graphStep; x += graphStep) {
+            let y = function(){return eval("with(storeVars(this)){" + g.replace(new RegExp(v, 'g'), x / graphScale) + "}")}.call(context) * graphScale
+            cvc.strokeStyle = "#f00"
+            cvc.moveTo(x, -y)
+            cvc.lineTo((x - graphStep), -l)
+            l = y
+            cvc.stroke()
+        }
+    } catch (e) {}
 }
 
-document.onkeydown = function(){
+document.onkeydown = function(e){
+    // Special keys
+    if (e && e.key == "Tab") {
+        e.preventDefault()
+        mode = (mode + 1) % 2
+        return
+    }
+
+    if (mode == 1) {
+
+        return
+    }
+
     // This timeout makes sure the code runs after the key is processed by the browser.
     setTimeout(function mathAnalyze(){
         let str = document.getElementById("edt").innerText
@@ -80,38 +115,43 @@ document.onkeydown = function(){
             } else if (str[s][str[s].length - 1] == '$') {
                 // Graph: Graphs things to a canvas.
                 graphs.push(parseMath(str[s].slice(0,-1)))
+                ov.children[s].innerHTML = "&nbsp;".repeat(str[s].length + 1) + "Graphed"
             } else {
                 // Didn't find anything to do.
                 ov.children[s].innerHTML = "&nbsp;" // If there's nothing (a normal space is nothing) the line doesn't render.
             }
         }
+        cv.width = cv.width
+        cvc.translate(Math.floor(cv.width / 2) + .5, Math.floor(cv.height / 2) + .5)
+        cvc.strokeStyle = "#000"
+        cvc.moveTo(cv.width / 2, 0)
+        cvc.lineTo(-cv.width / 2, 0)
+        cvc.stroke()
+        cvc.moveTo(0, cv.height / 2)
+        cvc.lineTo(0, -cv.height / 2 - .5)
+        cvc.stroke()
         if (graphs.length > 0) {
             cv.style.opacity = "1"
-            cv.width = cv.width
-            cvc.translate(Math.floor(cv.width / 2) + .5, Math.floor(cv.height / 2) + .5)
-            cvc.strokeStyle = "#000"
-            cvc.moveTo(cv.width / 2, 0)
-            cvc.lineTo(-cv.width / 2, 0)
-            cvc.stroke()
-            cvc.moveTo(0, cv.height / 2)
-            cvc.lineTo(0, -cv.height / 2 - .5)
-            cvc.stroke()
+            cv.style.backgroundColor = "rgba(255,255,255,1)"
             for (let g = 0; g < graphs.length; g++) {
                 graphs[g] = graphs[g].split('=')
                 if (graphs[g][0] == "y") {
-                    drawGraph(graphs[g][1], 'x')
+                    drawGraph(graphs[g][1], 'x', ctx)
                 }
             }
         } else {
             cv.style.opacity = "0"
+            cv.style.backgroundColor = "rgba(255,255,255,0)"
         }
     }, 0)
 }
+document.onpaste = document.onkeydown
 
 window.onresize = function() {
     cv.width = window.innerWidth / 2
     cv.height = window.innerHeight - 20
 }
 window.onresize()
+
 
 setTimeout(document.onkeydown, 100)
