b60a636ba9d5c6581eca17b1d728880845d19388
CodeIGuess <codeiguess@gmail.com>
2021-09-24 10:44:01 -0400

    I really this was already committed.
    I guess not.

--- ExtensionEDS/content.js
+++ ExtensionEDS/content.js
@@ -5,17 +5,26 @@ setTimeout(function(){
         return t
     })() == 1337 && document.getElementById("lblHeaderName")) {
         document.getElementById("lblHeaderName").onclick = function(){
-            if (document.getElementsByClassName("AnsOption").length != 0) {
-                alert("It's option #" + (Array.prototype.slice.call(document.getElementsByClassName("AnsOption")).map(function getAnswer(ctrl) {
-                    arrval = ctrl.value.split("#")
-                    arrkeyval = document.getElementById("hdnKeyValues").value.split("#")
-                    return arrval[2]
-                }).indexOf('1') + 1) + '!')
-            } else {
-                alert("Here is the solution :)")
-                el=document.getElementsByTagName("iframe")[0].contentDocument
-                try { el.getElementsByClassName("solutionMain")[0].style.display="inline-block" } catch(e) {}
-                try { el.getElementsByClassName("solutionMainSteps")[0].style.display="inline-block" } catch(e) {}
+            let foundSolution = false
+            for (var i = 0; i < parent.frames.length; i++) {
+                if ("showSolution" in parent.frames[i]) {
+                    parent.frames[i].showSolution()
+                    foundSolution = true
+                }
+            }
+            if (!foundSolution) {
+                if (document.getElementsByClassName("AnsOption").length != 0) {
+                    alert("It's option #" + (Array.prototype.slice.call(document.getElementsByClassName("AnsOption")).map(function getAnswer(ctrl) {
+                        arrval = ctrl.value.split("#")
+                        arrkeyval = document.getElementById("hdnKeyValues").value.split("#")
+                        return arrval[2]
+                    }).indexOf('1') + 1) + '!')
+                } else {
+                    alert("Here is the solution :)")
+                    el=document.getElementsByTagName("iframe")[0].contentDocument
+                    try { el.getElementsByClassName("solutionMain")[0].style.display="inline-block" } catch(e) {}
+                    try { el.getElementsByClassName("solutionMainSteps")[0].style.display="inline-block" } catch(e) {}
+                }
             }
         }
     }
