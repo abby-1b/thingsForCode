d01ed00819b9929c7cf6be87cf1b9ef0617c138b
CodeIGuess <codeiguess@gmail.com>
2022-02-14 13:31:13 -0400

    Using npm instead because I only need wat2wasm

--- MicroLang/wasmCompilerInstall.sh
+++ MicroLang/wasmCompilerInstall.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env zsh
 
-git clone --recursive https://github.com/WebAssembly/wabt
-cd wabt
-git submodule update --init
+# git clone --recursive https://github.com/WebAssembly/wabt
+# cd wabt
+# git submodule update --init
 
-mkdir build
-cd build
-cmake ..
-cmake --build .
+# mkdir build
+# cd build
+# cmake ..
+# cmake --build .
 
+npm i wat2wasm
