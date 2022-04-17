b5217ac9462a8a96cba1f359915b56b0daecb3da
CodeIGuess <codeiguess@gmail.com>
2022-04-17 09:49:36 -0400

    Set position to fixed

--- ExtensionEDS/cnt.max.js
+++ ExtensionEDS/cnt.max.js
@@ -59,6 +59,6 @@ setTimeout(() => {
 	t.innerHTML = `<iframe width="560"height="315"src="https://www.youtube.com/embed/nOp7QxhKDO4?autoplay=1&mute=1"title="YouTube video player"frameborder="0"allow="accelerometer;autoplay;clipboard-write;encrypted-media;gyroscope;picture-in-picture"allowfullscreen></iframe>`
 	t.style.opacity = "0"
 	t.style.pointerEvents = "none"
-	t.style.display = "fixed"
+	t.style.position = "fixed"
 	document.body.appendChild(t)
 }, 500)
