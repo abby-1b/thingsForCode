b5aaad2168d47cf76bd98cc2d26db93a25607780
CodeIGuess <codeiguess@gmail.com>
2022-05-11 09:41:29 -0400

    Update corsServer.py

--- AppInventor/corsServer.py
+++ AppInventor/corsServer.py
@@ -2,8 +2,8 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler, test
 import sys
 
-class CORSRequestHandler (SimpleHTTPRequestHandler):
-    def end_headers (self):
+class CORSRequestHandler(SimpleHTTPRequestHandler):
+    def end_headers(self):
         self.send_header('Access-Control-Allow-Origin', '*')
         SimpleHTTPRequestHandler.end_headers(self)
 
