13bf20e5c45438963f207e48f96f902fdc5ac3f0
CodeIGuess <codeiguess@gmail.com>
2022-05-29 15:40:19 -0400

    Update main.ts

--- NeuralNet/main.ts
+++ NeuralNet/main.ts
@@ -1,24 +1,31 @@
 
 import { NN } from "./nn.ts"
 
-let bot = new NN(2, 3, 1)
-let t = performance.now()
-bot.trainTo(0.1, [
-	[0, 0],
-	[0, 1],
-	[1, 0],
-	[1, 1]
-], [
-	[0],
-	[1],
-	[1],
-	[0]
-])
-console.log(performance.now() - t)
+// let n = Math.PI
+// let b = floatToBase(n)
+// let sn = baseToFloat(b)
 
-// console.log(bot.forward([0, 0]))
-// console.log(bot.forward([0, 1]))
-// console.log(bot.forward([1, 0]))
-// console.log(bot.forward([1, 1]))
+// console.log()
+// console.log(b)
+// console.log(sn)
+// console.log(n)
 
-console.log(bot.serialize())
+let bot = NN.from(Deno.readTextFileSync("XOR.nn"))
+// bot.trainTo(0.1, [
+// 	[0, 0],
+// 	[0, 1],
+// 	[1, 0],
+// 	[1, 1]
+// ], [
+// 	[0],
+// 	[1],
+// 	[1],
+// 	[0]
+// ])
+
+console.log(bot.forward([0, 0])[0])
+console.log(bot.forward([0, 1])[0])
+console.log(bot.forward([1, 0])[0])
+console.log(bot.forward([1, 1])[0])
+
+// console.log(bot.serialize())
