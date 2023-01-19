1241ab2f9d3b863e1a07673f620e60d2bdf84bb9
CodeIGuess <codeiguess@gmail.com>
2023-01-18 22:44:48 -0400

    Changed the approach to IO slightly

--- TinyLanguageJan2023/run.ts
+++ TinyLanguageJan2023/run.ts
@@ -1,23 +1,28 @@
+import { From, To } from "./genBytes.ts"
+
 const encoder = new TextEncoder()
 
-const mem = new Uint8Array(256)
+export const mem = new Uint8Array(256)
 const reg = new Uint32Array([
 	0, // 0x0 Math A
 	0, // 0x1 Math B
 	0, // 0x2 Math Op
 	0, // 0x3 Math Out
 	0, // 0x4 Addr
-	0, // 0x5 Memory
-	0, // 0x6 - 0x9 General purpose
+	0, // 0x5 Memory (8-bit)
+	0, // 0x6 Memory (16-bit)
+	0, // 0x7 Memory (32-bit)
+	0, // 0x8 - 0xC General purpose
 	0,
 	0,
 	0,
-	// 0xA - Log number/character (implementation dependant)
-	// 0xB - Jump if not zero
-	// 0xC - Jump
-	// 0xD - Read one byte
-	// 0xE - Read two bytes
-	// 0xF - Read four bytes
+	0,
+	// 0xD r - Read one byte
+	// 0xD  w- Log number/character (implementation dependant)
+	// 0xE r - Read two bytes
+	// 0xE  w- Jump if not zero
+	// 0xF r - Read four bytes
+	// 0xF  w- Jump
 ])
 let i = 0 // Instruction pointer
 
@@ -31,6 +36,11 @@ export function loadProgram(prg: number[]) {
 	for (; p < mem.length; p++) mem[p] = 0
 }
 
+function logIns(ins: number) {
+	const strNum = (n: number) => (n >> 4).toString(16) + (n & 15).toString(16)
+	console.log("[" + From[ins & 240] + " > " + To[ins & 15] + "]", strNum(ins), "[" + [...mem].splice(i + 1, 4).map(strNum).join(" ") + "]", "[" + [...reg].map(strNum).join(" ") + "]")
+}
+
 /**
  * Runs a cycle of the currently loaded program
  */
@@ -38,14 +48,20 @@ export function cycle() {
 	const ins = mem[i]
 		, t = ins & 15 // To reg
 	let f = ins >> 4 // From reg
-	// console.log(i, ins, f, t, " " + reg[3])
-	if (f >= 0xD) f = f == 0xD ? mem[++i] : f == 0xE ? mem[++i] << 8 | mem[++i] : mem[++i] << 16 | mem[++i] << 8 | mem[++i]
+	logIns(ins)
+	if (f >= 0xD) f = f == 0xD ? mem[++i]
+		: f == 0xE ? mem[++i] << 8 | mem[++i]
+		: mem[++i] << 24 | mem[++i] << 16 | mem[++i] << 8 | mem[++i]
 	else if (f == 0x5) f = mem[reg[0x4]]
+	else if (f == 0x6) f = mem[reg[0x4]] << 8 | mem[reg[0x4] + 1]
+	else if (f == 0x7) f = mem[reg[0x4]] << 24 | mem[reg[0x4] + 1] << 16 | mem[reg[0x4] + 2] << 8 | mem[reg[0x4] + 3]
 	else f = reg[f]
-	if (t == 0x5) mem[reg[4]] = f 
-	else if (t == 0xA) Deno.stdout.writeSync(encoder.encode(String.fromCharCode(f)))
-	else if (t == 0xB) i = reg[0x3] != 0 ? f - 1 : i
-	else if (t == 0xC) i = f - 1
+	if (t == 0x5) mem[reg[0x4]] = f
+	else if (t == 0x6) mem[reg[0x4]] = f >> 8, mem[reg[0x4] + 1] = f
+	else if (t == 0x7) mem[reg[0x4]] = f >> 24, mem[reg[0x4] + 1] = f >> 16, mem[reg[0x4] + 2] = f >> 8, mem[reg[0x4] + 3] = f
+	else if (t == 0xD) Deno.stdout.writeSync(encoder.encode(String.fromCharCode(f)))
+	else if (t == 0xE) i = reg[0x3] != 0 ? f - 1 : i
+	else if (t == 0xF) i = f - 1
 	else reg[t] = f
 	if (t < 4) switch (reg[2]) {
 		case  0: { reg[3] = reg[0] +  reg[1]         } break
