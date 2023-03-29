ca248e1f5d302635a46bf242a2e63688741089cb
CodeIGuess <codeiguess@gmail.com>
2023-03-28 22:59:24 -0400

    Fixed

--- Chess/new/src/mov/pawn.rs
+++ Chess/new/src/mov/pawn.rs
@@ -5,11 +5,13 @@ const IS_2 : u64 = 0x00FF000000000000;
 const IS_7 : u64 = 0x000000000000FF00;
 
 pub fn gen_moves(piece: u64, t_o: u64, o_o: u64, side: bool) -> u64 {
+	// TODO: fix pawns moving over pieces
+
 	return if !side {
 		((piece & NOT_A) >> 9 | (piece & NOT_H) >> 7)
-		& t_o | (((piece & IS_2) >> 16) | piece >> 8) & !(t_o | o_o)
+		& o_o | (((piece & IS_2) >> 16) | piece >> 8) & !(t_o | o_o)
 	} else {
 		((piece & NOT_A) << 9 | (piece & NOT_H) << 7)
-		& t_o | (((piece & IS_7) << 16) | piece << 8) & !(t_o | o_o)
+		& o_o | (((piece & IS_7) << 16) | piece << 8) & !(t_o | o_o)
 	}
 }
